3647d08f9de5a96328fa5e2bb192546ab3fe95dc
Twillyhub <44654308+Twillyhub@users.noreply.github.com>
2019-02-25 13:32:00 -0600

    Add files via upload

---
+++ animate.js
@@ -0,0 +1,12 @@
+
+var menu = {};
+
+menu = {
+    "Spicy Chicken wing": "htis is wing des",
+    "Chicken w/ snow peas": "super good chicken",
+    "Chicken w/mushrooms": "yummy mushrooms"
+}
+
+function lookUp(){
+var x = document.getElementById("mySearch").placeholder;
+}
